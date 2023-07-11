f8d5703a3aa9aa44ded59c65ceae3a03c4b40b6b
princysingh2409@gmail.com <princysingh2409@gmail.com>
2023-07-11 12:13:44 +0530

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp4
 {
+   public class Emp
+    {
+        public string FullName(string fname, string lname)
+        {
+            return fname = " " + lname;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Emp emp = new Emp();
+            Console.WriteLine(emp.FullName("Princy", "Singh"));
+            Console.ReadKey();
         }
     }
 }
